245db6bec7d8f708b5463fa8bc93547eee5f06ce
Eric Betts <bettse@fastmail.fm>
2017-04-12 14:45:10 -0700

    Switch to debug, copy over decode function

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var noble = require('noble');
 var protobuf = require("protobufjs");
+var debug = require('debug')('spec_client');
 const serviceUUID = '3e400001b5a3f393e0a9e50e24dcca9e'
 const MAX_CHARACTERISTIC_SIZE = 20
+const headerHex = '200000';
 
 const responses = [
   new Buffer('200000470a450801124104ba9cb363577a8e21555f34e72feb37394f59e3216c46b10a5547d50bdbc89877177045474cccdf07f6e144aedbf8bc997da7ec8871a0b7144d877a0f8cdab128','hex'),
-  new Buffer('2000005e0a5c08031258cd5e310a0d2e47dba288327c778870ad4b50c80087ef2b4e0e18cc948e36622b9d76bd568a6509b888059d57a030ae51e360e7e32b159cc873e08ed198a66d74d8bc12ef0f786610b9a054effc8ef88a026028487f81d369', 'hex')
+  new Buffer('2000005e0a5c08031258cd5e310a0d2e47dba288327c778870ad4b50c80087ef2b4e0e18cc948e36622b9d76bd568a6509b85b61730adea1a719e9b62a343e1094c3cadd92b76f8158357ab4abd8a75b29cb7dd541ced8a1a929629917dac0b13ebc', 'hex')
 ];
 
 var writeCharacteristic = null;
@@ -16,7 +18,7 @@ const modes = {
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
-    console.log(state, 'startScanning')
+    debug(state, 'startScanning')
     noble.startScanning();
   } else {
     noble.stopScanning();
@@ -25,19 +27,19 @@ noble.on('stateChange', function(state) {
 
 noble.on('discover', function(peripheral) {
   if (peripheral.advertisement.manufacturerData && peripheral.advertisement.manufacturerData.compare(modes['pair']) === 0) {
-    console.log('peripheral found', peripheral.advertisement)
+    debug('peripheral found', peripheral.advertisement)
     noble.stopScanning();
 
     peripheral.on('disconnect', function() {
-      console.log('disconnected, exiting');
+      debug('disconnected, exiting');
       process.exit(0);
     });
 
     peripheral.connect(function(error) {
-      console.log('connected');
+      debug('connected');
       peripheral.discoverServices([serviceUUID], function(error, services) {
         services.forEach(function(service) {
-          console.log('service', service.uuid);
+          debug('service', service.uuid);
           service.discoverCharacteristics([], handleCharacteristics);
         });
       });
@@ -46,7 +48,6 @@ noble.on('discover', function(peripheral) {
 });
 
 function sendMessage(message) {
-  console.log('sendMessage', message.toString('hex'));
   var cursor = 0;
   var end, chunk;
 
@@ -55,27 +56,25 @@ function sendMessage(message) {
       end = Math.min(cursor + MAX_CHARACTERISTIC_SIZE, message.length);
       chunk = message.slice(cursor, end);
       cursor = end;
-      console.log("\tsendChunk", chunk.toString('hex'));
       writeCharacteristic.write(chunk, false, sendChunk);
     }
   }
 
+  debug('=>');
+  decode(message);
   sendChunk();
 }
 
 function handleCharacteristics(error, characteristics) {
   characteristics.forEach(function(characteristic) {
     if (characteristic.properties.includes('write')) {
-      console.log('characteristic', characteristic.uuid, 'saved');
       writeCharacteristic = characteristic;
     } else if (characteristic.properties.includes('indicate')) {
       characteristic.on('data', readChunk);
-      console.log('characteristic', characteristic.uuid, 'subscribed');
       characteristic.subscribe();
     }
   });
 
-  console.log('-----');
   if (writeCharacteristic) {
     sendMessage(responses[0]);
   }
@@ -84,8 +83,6 @@ function handleCharacteristics(error, characteristics) {
 var incompleteData = Buffer.alloc(0);
 var bytesRemaining = 0;
 function readChunk(chunk, isNotification) {
-  console.log("\treadChunk", chunk.toString('hex'));
-
   if (incompleteData && incompleteData.length > 3) {
     var totalLength = incompleteData[3] + 4;
     var bytesRemain = totalLength - incompleteData.length;
@@ -107,20 +104,34 @@ function readChunk(chunk, isNotification) {
     // This glosses over when the chunk has multiple messages < 20 bytes
     incompleteData = Buffer.concat([incompleteData, chunk]);
   }
-
-  if (incompleteData.length > 0) {
-    console.log('incompleteData', incompleteData.toString('hex'));
-  }
 }
 
 function completeMessage(buffer) {
-  console.log('completeMessage', buffer.toString('hex'));
-  protobuf.load("laguna.proto", function withProtocol(err, root) {
-    var Envelope = root.lookupType("laguna.Envelope");
-    var decodedMessage = Envelope.decode(buffer.slice(4));
-    console.log('decoded', decodedMessage);
-  });
-  if (buffer[3] == 0x20) {
-    sendMessage(responses[1]);
-  }
+  debug('<=');
+	decode(buffer).then(function(messageList) {
+    messageList.forEach(function(decodedMessage) {
+      if (decodedMessage.a.a === 2) {
+			  sendMessage(responses[1]);
+      }
+	  });
+	});
 }
+
+function decode(buffer) {
+  return protobuf.load("laguna.proto").then(function(root) {
+    var Envelope = root.lookupType("laguna.Envelope");
+    var lastIndex = 0;
+    var list = [];
+    debug('raw', buffer.toString('hex'));
+    do {
+      lastIndex += 3;//header
+      var message = buffer.slice(lastIndex);
+      var decodedMessage = Envelope.decodeDelimited(message);
+      debug('decodedMessage', decodedMessage);
+      list.push(decodedMessage);
+      lastIndex = buffer.indexOf(headerHex, lastIndex, 'hex')
+    } while (lastIndex != -1)
+
+    return list;
+  });
+};
